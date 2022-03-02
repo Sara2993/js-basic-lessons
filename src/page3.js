adc6e60ed50570429884b7cc1c31af5fac7997cf
Saraswathi M <sara2993@global.chainsys.com>
2022-03-02 18:18:14 +0530

    :include index file

---
+++ src/page3.js
@@ -0,0 +1,13 @@
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+function addNumbers (a, b) {
+  return a + b;
+}
+// eslint-disable-next-line eol-last
+console.log(`2 + 3 => ${addNumbers(2, 3)}`);
+console.log('multiplyNumbers(20,3)');
+
+function multiplyNumbers (a, b) {
+  return a * b;
+}
+console.log(`2 * 3 => ${multiplyNumbers(2, 3)}`);
